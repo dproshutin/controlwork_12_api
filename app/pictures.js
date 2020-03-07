ce4bb39f164d7dc4475e3399a4e16b3474016858
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2020-03-07 13:55:36 +0600

    Добавлены методы получения всех фотографий из галереи и получения конкретной фотографии по id.

--- app/pictures.js
+++ app/pictures.js
@@ -2,7 +2,27 @@ const express = require("express");
 const router = express.Router();
 
 const createRouter = () => {
+    router.get("/", async (req, res) => {
+        try {
+            let pictures = await Picture.find().populate('createdBy');
+            if (pictures.length > 0) {
+                if (req.query.user) {
+                    pictures = pictures.filter(item => item.createdBy._id.toString() === req.query.user);
+                }
+            }
+            res.send(pictures);
+        } catch {
+            res.sendStatus(500);
+        }
+    });
 
+    router.get('/:id', async (req, res) => {
+        const criteria = {_id: req.params.id};
+        await Picture.findOne(criteria).then(picture => {
+            if (picture) res.send(picture);
+            else res.sendStatus(404);
+        }).catch(() => res.sendStatus(500));
+    });
 
     return router;
 };
